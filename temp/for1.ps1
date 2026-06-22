8b6764c15b0d422bc08647177aeaf1f14ae81291
Antonio José Yuste López <yusteibiza@hotmail.com>
2026-06-22 11:04:27 +0200

    Añadida la carpeta Temp

--- temp/for1.ps1
+++ temp/for1.ps1
@@ -3,22 +3,29 @@
 ## ESC[?25h	mostrar cursor ##
 #############################
 
-$color = ""
+param(
+    [int]$inicio = 1,
+    [int]$fin = 100,
+    [int]$delay = 50
+)
 
-Write-Host "`nPrueba de bucle for...`n" -foreground green
+$color = "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray",
+"Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White"
 
-for ($i = 1; $i -le 100; $i++) {
-    $color = "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray",
-    "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White" | Get-Random
+clear
+Write-Host "`nPrueba de bucle for...`n" -foreground white
+
+for ($i = $inicio; $i -le $fin; $i++) {
+    $c = Get-Random $color
     
-    if ($i -eq 100) {
-        Write-Host "`e[?25l$i`n" -NoNewline -foreground $color;
+    if ($i -eq $fin) {
+        Write-Host "`e[?25l$i`n" -NoNewline -foreground $c;
         Start-Sleep 0.1
     } else {
-        Write-Host "`e[?25l$i, " -NoNewline -foreground $color;
+        Write-Host "`e[?25l$i, " -NoNewline -foreground $c;
     }
 
-    Start-Sleep 0.1
+    Start-Sleep -Milliseconds $delay
 } 
 
 Write-Host "`e[?25h"
